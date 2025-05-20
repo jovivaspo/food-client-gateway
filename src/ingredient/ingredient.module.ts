377dee2c88f5853e3546edb261a3288c66206217
jovivaspo <jorgevipo.dev@gmail.com>
2025-05-20 10:28:49 +0200

    feat: delete ingredient table

--- src/ingredient/ingredient.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NatsModule } from 'src/nats/nats.module';
-import { IngredientController } from './ingredient.controller';
-
-@Module({
-  controllers: [IngredientController],
-  imports: [NatsModule],
-})
-export class IngredientModule {}
